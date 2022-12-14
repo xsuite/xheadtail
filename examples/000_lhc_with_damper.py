c868939643b137c23b4afd89ca8b36c683a95d67
giadarol <giovanni.iadarola@cern.ch>
2022-12-14 21:48:21 +0100

    Example

--- examples/000_lhc_with_damper.py
+++ examples/000_lhc_with_damper.py
@@ -6,33 +6,36 @@ import xtrack as xt
 import xpart as xp
 import xheadtail as xht
 
+# Load a line
 fname_line = '../../xtrack/test_data/lhc_no_bb/line_and_particle.json'
 with open(fname_line, 'r') as fid:
      input_data = json.load(fid)
 line = xt.Line.from_dict(input_data['line'])
 line.particle_ref = xp.Particles(p0c=7e12)
 
+# Install a damper
 line.append_element(
     element=xht.TransverseDamper(damping_time_x=10., damping_time_y=15.),
     name='damper'
 )
 
+# Build the tracker
 tracker = line.build_tracker()
 
+# Generate a matched bunch
 particles = xp.generate_matched_gaussian_bunch(
     tracker=tracker, nemitt_x=2.5e-6, nemitt_y=2.5e-6,
     total_intensity_particles=1e11,
     num_particles=200, sigma_z=0.1)
 
+# Kick the bunch
 particles.x += 0.5e-3
 particles.y += 1e-3
 
+# Track!
 tracker.track(particles, num_turns=100, turn_by_turn_monitor=True)
 
-########
-# Plot #
-########
-
+# Plot
 res = tracker.record_last_track
 
 import matplotlib.pyplot as plt
